e129d62ce3010f9522e8f40452f0c7bc69adf94e
jdarmada <jdarmada@gmail.com>
2024-01-03 17:21:57 -0800

    added descriptions and partial instructions

--- src/app/demo-container/demo-item/demo-item.component.ts
+++ src/app/demo-container/demo-item/demo-item.component.ts
@@ -10,21 +10,25 @@ export class DemoItemComponent {
 
     {
       title: 'Opening Extension',
+      description: "Starting Source Directory",
       gifContent: 'gifLink' ,
-      instructionsList: []
+      instructionsList: ["Install AnguLens from extension marketplace", "Open commands (cmd/ctrl + shift + p) and run \"Start AnguLens\"", "Right click on \"src\" folder in Angular project", "Click \"Copy Path\"", "Paste path into text input and click generate"]
     },
     {
       title: 'Folder-File View',
+      description: "Displays file structure of selected directory",
       gifContent: 'gifLink' ,
       instructionsList: []
     },
     {
       title: 'Component View',
+      description: "Displays component hierarchy, including routers, with overlayed connections representing different connections",
       gifContent: 'gifLink' ,
       instructionsList: []
     },
     {
       title: 'Services View',
+      description: "Displays every service as a new network, with any components that utilize that service",
       gifContent: 'gifLink' ,
       instructionsList: []
     }
