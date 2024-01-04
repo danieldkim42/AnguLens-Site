a2186e4969ce81f4b88fb437a26d5f4c949adfb3
jdarmada <jdarmada@gmail.com>
2024-01-03 16:54:32 -0800

    added demoItems

--- src/app/demo-container/demo-item/demo-item.component.ts
+++ src/app/demo-container/demo-item/demo-item.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo-item.component.scss']
 })
 export class DemoItemComponent {
+  demoItems = [
+
+    {
+      title: 'Opening Extension',
+      gifContent: 'gifLink' ,
+      instructionsList: []
+    },
+    {
+      title: 'Folder-File View',
+      gifContent: 'gifLink' ,
+      instructionsList: []
+    },
+    {
+      title: 'Component View',
+      gifContent: 'gifLink' ,
+      instructionsList: []
+    },
+    {
+      title: 'Services View',
+      gifContent: 'gifLink' ,
+      instructionsList: []
+    }
+    
+
+  ]
 
 }
